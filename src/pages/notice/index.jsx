7db6020f1090610d124691dfc2e6a70a54379b33
Boin Kim <charles1236@kau.kr>
2022-06-09 14:55:16 +0900

    fix: 알림 페이지 bottomNav openStatus false 적용

--- src/pages/notice/index.jsx
+++ src/pages/notice/index.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil';
+import { bottomNavStatusState } from '../../recoil/BottomNav';
 
 export default function Notice() {
+  const setBottomNavStatus = useSetRecoilState(bottomNavStatusState);
+
+  useEffect(() => {
+    setBottomNavStatus(false);
+
+  });
+
   return (
     <div>Notice</div>
   )
