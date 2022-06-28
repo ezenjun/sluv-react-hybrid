acc5e16f7e28743a9ecb061cff91055a19aad1b0
ezenjun <ezenjun07@gmail.com>
2022-06-29 00:48:22 +0900

    feat: Home navbar, tab 구현

--- src/pages/home/Custom.jsx
+++ src/pages/home/Custom.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function Custom() {
-  return (
-    <div>Event</div>
-  )
+	return <div>Custom</div>;
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,33 +1,64 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import { ReactComponent as Logo } from '../../assets/Logo/LogoHome.svg';
+import { ReactComponent as Alarm } from '../../assets/Icons/alarm.svg';
+import { MainContainer } from '../../components/containers/MainContainer';
+import { TopNav } from '../../components/containers/TopNav';
 
 export default function Home() {
-
+	const [tabIndex, setTabIndex] = useState(1);
+	const toggleTab = idx => {
+		setTabIndex(idx);
+		console.log(tabIndex);
+	};
 	useEffect(() => {
 		// 하단바 띄워주기
-		
 	}, []);
 
 	return (
 		<>
 			<MainContainer>
-				<Header>
-					<StyledLink to="">SLUV</StyledLink>
-					<StyledLink to="../notice">알림</StyledLink>
-				</Header>
+				<TopNav style={{ justifyContent: 'space-between' }}>
+					<LogoWrap to="">
+						<Logo />
+					</LogoWrap>
+					<NavRight to="../notice">
+						<Alarm />
+					</NavRight>
+				</TopNav>
 				<TabContainer>
-					<StyledLink to="">
-						<Tab>맞춤</Tab>
+					<StyledLink
+						to=""
+						margin="0 20px 0 0"
+						borderBottom={tabIndex === 1 ? '2px solid #262626' : ''}
+						onClick={() => toggleTab(1)}
+					>
+						<Tab color={tabIndex === 1 ? '#262626' : ''}>맞춤</Tab>
 					</StyledLink>
-					<StyledLink to="follow">
-						<Tab>팔로잉</Tab>
+					<StyledLink
+						to="follow"
+						margin="0 20px 0 0"
+						borderBottom={tabIndex === 2 ? '2px solid #262626' : ''}
+						onClick={() => toggleTab(2)}
+					>
+						<Tab color={tabIndex === 2 ? '#262626' : ''}>팔로잉</Tab>
 					</StyledLink>
-					<StyledLink to="question">
-						<Tab>질문</Tab>
+					<StyledLink
+						to="question"
+						margin="0 20px 0 0"
+						borderBottom={tabIndex === 3 ? '2px solid #262626' : ''}
+						onClick={() => toggleTab(3)}
+					>
+						<Tab color={tabIndex === 3 ? '#262626' : ''}>질문</Tab>
 					</StyledLink>
-					<StyledLink to="event">
-						<Tab>이벤트</Tab>
+					<StyledLink
+						to="event"
+						margin="0 20px 0 0"
+						borderBottom={tabIndex === 4 ? '2px solid #262626' : ''}
+						onClick={() => toggleTab(4)}
+					>
+						<Tab color={tabIndex === 4 ? '#262626' : ''}>이벤트</Tab>
 					</StyledLink>
 				</TabContainer>
 				<FeedContainer>
@@ -38,49 +69,43 @@ export default function Home() {
 	);
 }
 
-const MainContainer = styled.div`
-	height: 100vh;
+const LogoWrap = styled(Link)`
 	display: flex;
-	flex-direction: column;
-	overflow-y: scroll;
-	padding: 1rem;
 `;
 
-const Header = styled.div`
+const NavRight = styled(Link)`
 	display: flex;
-	height: 60px;
-	background-color: pink;
-	justify-content: space-between;
-	align-items: center;
+	justify-content: flex-end;
 `;
 const TabContainer = styled.div`
 	display: flex;
-	height: 50px;
-	background-color: skyblue;
-	align-items: center;
+	padding: 0.6875rem 1.25rem 0 1.25rem;
 `;
 
-const Tab = styled.h4`
-	margin-right: 20px;
-	text-decoration: none;
-	color: white;
+const Tab = styled.div`
+	box-sizing: border-box;
+	color: ${props => props.color || '#8d8d8d'};
+	margin-bottom: 0.6875rem;
+	font-size: 1.125rem;
 	font-weight: bold;
+	font-stretch: normal;
+	font-style: normal;
+	line-height: normal;
+	letter-spacing: normal;
+	text-align: left;
 `;
 
 const FeedContainer = styled.div`
 	height: 100vh;
+	overflow-y: scroll;
 	border: 1px solid black;
-	background-color: orange;
 `;
 
 const StyledLink = styled(Link)`
+	margin: 0;
+	padding: 0;
 	text-decoration: none;
-	color: black;
-	&:focus,
-	&:hover,
-	&:visited,
-	&:link,
-	&:active {
-		text-decoration: none;
-	}
+	color: ${props => props.margin || '#8d8d8d'};
+	margin: ${props => props.margin || '0'};
+	border-bottom: ${props => props.borderBottom || 'none'};
 `;
