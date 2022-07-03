c223563e05c68e0a6923c7b85bf6db854b09e8f2
ezenjun <ezenjun07@gmail.com>
2022-07-03 22:52:45 +0900

    feat: 바인더 기본페이지 레이아웃 완성

--- src/pages/binder/sampleItems.js
+++ src/pages/binder/sampleItems.js
@@ -1,22 +1,6 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
 export default [
 	{
-		src: 'https://images.pexels.com/photos/2575279/pexels-photo-2575279.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-		id: '8c444080-9e02-4946-9205-ea65ed81a66f',
-	},
-	{
-		src: 'https://images.pexels.com/photos/1029609/pexels-photo-1029609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-		id: '0a62f9fa-6f5c-4b09-8ff0-d2b9169426f9',
-	},
-	{
-		src: 'https://images.pexels.com/photos/1122414/pexels-photo-1122414.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-		id: '308f0373-a548-4802-8824-a1429ed3e801',
-	},
-	{
-		src: 'https://images.pexels.com/photos/2698761/pexels-photo-2698761.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-		id: '3c36057f-11de-4fc0-bc52-334c2339741a',
-	},
-	{
 		src: 'https://images.pexels.com/photos/3075988/pexels-photo-3075988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
 		id: '1',
 	},
@@ -35,5 +19,21 @@ export default [
 	{
 		src: 'https://images.pexels.com/photos/1084188/pexels-photo-1084188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 		id: '5',
+	},
+	{
+		src: 'https://images.pexels.com/photos/2575279/pexels-photo-2575279.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		id: '6',
+	},
+	{
+		src: 'https://images.pexels.com/photos/1029609/pexels-photo-1029609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		id: '7',
+	},
+	{
+		src: 'https://images.pexels.com/photos/1122414/pexels-photo-1122414.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		id: '8',
+	},
+	{
+		src: 'https://images.pexels.com/photos/2698761/pexels-photo-2698761.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		id: '9',
 	},
 ];
