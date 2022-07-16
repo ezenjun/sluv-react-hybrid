3106d50a05ab2074332d41e30de83450ce06c257
Boin Kim <charles1236@kau.kr>
2022-07-17 06:36:11 +0900

    fix: 마이페이지&프로필 > 팔로잉, 팔로우 없을 때 화면 세팅

--- src/pages/my/ViewedItems.jsx
+++ src/pages/my/ViewedItems.jsx
@@ -42,7 +42,7 @@ export default function ViewedItems() {
 	useEffect(() => {
 		setBottomNavStatus(false);
 
-		getViewItems();
+		// getViewItems();
 	}, []);
 
 	const getViewItems = async () => {
