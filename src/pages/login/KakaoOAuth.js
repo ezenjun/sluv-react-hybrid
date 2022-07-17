9c81313aed4b50d5f4dc043bb7212182f8041096
Boin Kim <charles1236@kau.kr>
2022-07-17 16:05:33 +0900

    fix: 카카오톡 리다이렉트 URI 수정

--- src/pages/login/KakaoOAuth.js
+++ src/pages/login/KakaoOAuth.js
@@ -1,4 +1,4 @@
 const CLIENT_ID = '203e7070b221e304d3ea7500a88f9475';
-const REDIRECT_URI = 'http://sluv.co.kr/auth/kakao-login';
+const REDIRECT_URI = 'https://sluv.co.kr/auth/kakao-login';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
