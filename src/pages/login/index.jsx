df79b1c921179575981c4c7c9ee6f193edc1009a
Boin Kim <charles1236@kau.kr>
2022-07-17 14:58:44 +0900

    fix: 메인에서 작업해서 죄송합니다..

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -48,15 +48,19 @@ export default function Login() {
 		const data = await customApiClient('get', '/auth/auto-login');
 		console.log(data);
 		if(!data) return;
-		if(!data.isSuccess) return;
+		
+		if(!data.isSuccess) {
+			console.log('자동로그인 X');
+		}
+
 
 		if(data.code === 1001) {
+			localStorage.setItem('myUserIdx', data.result.userIdx);
 			navigate('/home');
 		}
 	}
 
 	const getMyFcmToken = async () => {
-
 		localStorage.removeItem('isFcmLoad');
 		let localFcm = localStorage.getItem('fcmToken');
 
@@ -65,6 +69,18 @@ export default function Login() {
 		}
 
 		const current_user_platform = checkMobile;
+
+		//앱 버전관련 로직
+		// try {
+		// 	if (current_user_platform == 'android') {
+		// 		const verson = await window.android.getVersionName();
+		// 		localStorage.setItem('versonName', verson);
+		// 	} else if (current_user_platform == 'ios') {
+		// 		window.webkit.messageHandlers.getVersionName.postMessage('hihi');
+		// 	}
+		// } catch (err) {
+		// 	localStorage.removeItem('versonName');
+		// }
 
 		if (current_user_platform == 'android' && !localFcm) {
 			try {
