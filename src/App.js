e499aa2469b79a714abe319cae57aa7e9b5732ef
ezenjun <ezenjun07@gmail.com>
2022-07-27 15:06:49 +0900

    feat: 회원탈퇴

--- src/App.js
+++ src/App.js
@@ -14,21 +14,23 @@ import {
 
 //모바일 기기 체크
 export const checkMobile = () => {
+	var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
 
-  var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
-
-  if (varUA.indexOf('android') > -1) {
-    //안드로이드
-    return "android";
-  } else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
-    //IOS
-    return "ios";
-  } else {
-    //아이폰, 안드로이드 외
-    return "other";
-  }
-
-}
+	if (varUA.indexOf('android') > -1) {
+		//안드로이드
+		return 'android';
+	} else if (
+		varUA.indexOf('iphone') > -1 ||
+		varUA.indexOf('ipad') > -1 ||
+		varUA.indexOf('ipod') > -1
+	) {
+		//IOS
+		return 'ios';
+	} else {
+		//아이폰, 안드로이드 외
+		return 'other';
+	}
+};
 
 export default function App() {
 	const toastMessageStatus = useRecoilValue(ToastMessageStatusState);
@@ -36,7 +38,6 @@ export default function App() {
 	const message = useRecoilValue(ToastMessageState);
 	const bottomPosition = useRecoilValue(ToastMessageBottomPositionState);
 
-	
 	return (
 		<RootWrap>
 			<BrowserRouter>
@@ -101,4 +102,3 @@ const ToastMessage = styled.div`
 	transition: opacity 300ms ease-out;
 	opacity: ${props => (props.openStatus ? '1' : '0')};
 `;
-
