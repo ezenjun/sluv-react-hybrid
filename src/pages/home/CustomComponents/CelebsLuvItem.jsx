89a4ced496349ca880eab0f8d8ac775b16e718d8
ezenjun <ezenjun07@gmail.com>
2022-07-29 21:11:03 +0900

    feat: 이벤트 페이지 사진 수정 및 인스타그램 버튼 추가

--- src/pages/home/CustomComponents/CelebsLuvItem.jsx
+++ src/pages/home/CustomComponents/CelebsLuvItem.jsx
@@ -834,20 +834,6 @@ const Image = styled.div`
 		display: block;
 		padding-bottom: 100%;
 	}
-	:empty {
-		background: linear-gradient(to right, #eeeeee 10%, #dddddd 28%, #eeeeee 53%);
-		background-size: 100%;
-		animation: 2s loadingAnimation forwards infinite linear; //애니메이션 적용
-	}
-	@keyframes loadingAnimation {
-		//배경색 위치 이동하는 애니메이션
-		0% {
-			background-position: -20rem 0;
-		}
-		100% {
-			background-position: 20rem 0;
-		}
-	}
 `;
 
 const ImageText = styled.div`
